323d450d5970be980673d1f4b62f073a8e1c2f05
PeaceA <peace.aku@bison.howard.edu>
2018-11-28 18:34:15 -0800

    LevelThree

---
+++ app/src/main/java/com/techexchange/mobileapps/numbahunt/LevelFour.java
@@ -0,0 +1,59 @@
+package com.techexchange.mobileapps.numbahunt;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LevelFour extends AppCompatActivity {
+    String inputAnswer = "";
+    String correctAnswer = "369";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_four);
+    }
+
+
+    public void buttonClicked (View view) {
+
+        Button b = (Button)view;
+        String buttonText = b.getText().toString();
+        inputAnswer += buttonText;
+
+        TextView answer = findViewById(R.id.userAnswer);
+        answer.setText(String.valueOf(inputAnswer));
+    }
+
+    public void decrementString (View view) {
+
+        if(inputAnswer != "") {
+            inputAnswer = inputAnswer.substring(0, inputAnswer.length() - 1);
+        }
+
+        TextView answer = findViewById(R.id.userAnswer);
+        answer.setText(String.valueOf(inputAnswer));
+    }
+
+    public void correctString (View view) {
+
+        if (inputAnswer.equals(correctAnswer)) {
+            Toast.makeText(this, "Congrats!", Toast.LENGTH_LONG).show();
+
+            //Intent nextLevel1 = new Intent(LevelFour.this, LevelFive.class);
+            //startActivity(nextLevel1);
+        }
+        else {
+            Toast.makeText(this,"Sorry Try Again", Toast.LENGTH_LONG).show();
+
+            inputAnswer = "";
+            TextView answer = findViewById(R.id.userAnswer);
+            answer.setText(String.valueOf(inputAnswer));
+        }
+
+    }
+}
